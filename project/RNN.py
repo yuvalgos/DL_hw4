a48e1320e29d75690779c63e3c41d9a67b8f202b
yuvalgos <yuvalgos@gmail.com>
2021-08-19 18:39:14 +0300

    Implemented baseline model of GRU and created basic training code

--- project/RNN.py
+++ project/RNN.py
@@ -1,54 +1,45 @@
+import numpy as np
 import torch
 import torch.nn as nn
 
 
-class RNNLayer(nn.Module):
-    def __init__(self, in_dim, h_dim, out_dim, phi_h=torch.tanh, phi_y=torch.sigmoid):
-        super().__init__()
-        self.phi_h, self.phi_y = phi_h, phi_y
-
-        self.fc_xh = nn.Linear(in_dim, h_dim, bias=False)
-        self.fc_hh = nn.Linear(h_dim, h_dim, bias=True)
-        self.fc_hy = nn.Linear(h_dim, out_dim, bias=True)
-
-    def forward(self, xt, h_prev=None):
-        if h_prev is None:
-            h_prev = torch.zeros(xt.shape[0], self.fc_hh.in_features)
-
-        ht = self.phi_h(self.fc_xh(xt) + self.fc_hh(h_prev))
-
-        yt = self.fc_hy(ht)
-
-        if self.phi_y is not None:
-            yt = self.phi_y(yt)
-
-        return yt, ht
-
-
-class SentimentRNN(nn.Module):
-    def __init__(self, vocab_dim, embedding_dim, h_dim, out_dim):
+class SentimentGRU(nn.Module):
+    def __init__(self, embeddings: np.ndarray, num_classes=3,hidden_size=128, num_layers=2,
+                 dropout=.0, freeze_embedding=True):
         super().__init__()
 
-        # nn.Embedding converts from token index to dense tensor
-        self.embedding = nn.Embedding(vocab_dim, embedding_dim)
+        self.vocab_size = embeddings.shape[0]
+        self.embedding_dim = embeddings.shape[1]
 
-        # Our own Vanilla RNN layer, without phi_y so it outputs a class score
-        self.rnn = RNNLayer(in_dim=embedding_dim, h_dim=h_dim, out_dim=out_dim, phi_y=None)
+        self.embedding_layer = torch.nn.Embedding.from_pretrained(
+            torch.from_numpy(embeddings).float())
+        if freeze_embedding:
+            self.embedding_layer.weight.requires_grad = False
+
+        self.gru = torch.nn.GRU(input_size=self.embedding_dim,
+                                hidden_size=hidden_size,
+                                num_layers=num_layers,
+                                dropout=dropout)
+
+        # GRU output shape after taking last layer at last time step is (B, hidden_size),
+        # we need a dense layer to get class scores from it
+        self.dense_linear = torch.nn.Linear(hidden_size, num_classes)
 
         # To convert class scores to log-probability we'll apply log-softmax
         self.log_softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, X):
-        # X shape: (S, B) Note batch dim is not first!
+        # X shape is (S, B). contains tokens
+        X_embedded = self.embedding_layer(X)
 
-        embedded = self.embedding(X)  # embedded shape: (S, B, E)
+        # GRU input should be (S, B, E)
+        _, h_n= self.gru(X_embedded)
 
-        # Loop over (batch of) tokens in the sentence(s)
-        ht = None
-        for xt in embedded:  # xt is (B, E)
-            yt, ht = self.rnn(xt, ht)  # yt is (B, D_out)
+        # we only need the last hidden state at the last time step
+        class_scores = self.dense_linear(h_n[-1, :, :])
 
-        # Class scores to log-probability
-        yt_log_proba = self.log_softmax(yt)
+        log_prob = self.log_softmax(class_scores)
+        sentiment_class = torch.argmax(class_scores, dim=1)
 
-        return yt_log_proba
+        # log prob shape is(B, C)
+        return sentiment_class, log_prob
